79ed766af430865bf69e6137302fcc432c5e7570
aaliya khan <khanaaliya0277@gmail.com>
2025-02-11 21:23:41 +0530

    ak

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //
 let roll = () => {
+  let rollSound = new Audio("./dice_roll.mp3");  // Add a dice sound file in your project folder
+  rollSound.play();
+  
+
   let dice = Math.floor(Math.random() *6 + 1);
   let box1 = document.querySelector("#box1");
   let box2 = document.querySelector("#box2");
@@ -7,7 +11,8 @@ let roll = () => {
   let box4 = document.querySelector("#box4");
   let box5 = document.querySelector("#box5");
   let box6 = document.querySelector("#box6");
-  
+  let resultText = document.getElementById("result");
+
   if (dice == 1) {
     box1.style.opacity = "1";
     box2.style.opacity = "0";
@@ -15,6 +20,8 @@ let roll = () => {
     box4.style.opacity = "0";
     box5.style.opacity = "0";
     box6.style.opacity = "0";
+    resultText.innerText = "You rolled a 1!🙂";
+
   } 
   else if (dice == 2) {
     box1.style.opacity = "0";
@@ -23,6 +30,8 @@ let roll = () => {
     box4.style.opacity = "0";
     box5.style.opacity = "0";
     box6.style.opacity = "0";
+    resultText.innerText = "You rolled a 2!😊";
+
   }
    else if (dice == 3) {
     box1.style.opacity = "0";
@@ -31,6 +40,8 @@ let roll = () => {
     box4.style.opacity = "0";
     box5.style.opacity = "0";
     box6.style.opacity = "0";
+    resultText.innerText = "You rolled a 3!😀";
+
   }
   else if (dice == 4) {
     box1.style.opacity = "0";
@@ -39,6 +50,8 @@ let roll = () => {
     box4.style.opacity = "1";
     box5.style.opacity = "0";
     box6.style.opacity = "0";
+    resultText.innerText = "You rolled a 4!😄";
+
   }
   else if (dice == 5) {
     box1.style.opacity = "0";
@@ -47,6 +60,8 @@ let roll = () => {
     box4.style.opacity = "0";
     box5.style.opacity = "1";
     box6.style.opacity = "0";
+    resultText.innerText = "You rolled a 5!😇";
+
   }
   else if (dice == 6) {
     box1.style.opacity = "0";
@@ -55,6 +70,8 @@ let roll = () => {
     box4.style.opacity = "0";
     box5.style.opacity = "0";
     box6.style.opacity = "1";
+    resultText.innerText = "👑You rolled a 6!👑";
+
   }
 
 }
